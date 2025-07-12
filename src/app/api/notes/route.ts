b35ae42a082a6b1077f3fef2d1485f2e0637188e
Luis Girao <luis.oscar.girao@gmail.com>
2025-07-12 22:51:25 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -1,7 +1,7 @@
 
 'use server';
 import { NextResponse } from 'next/server';
-import prisma from '@/lib/prisma';
+import { prisma } from '@/lib/prisma';
 import { z } from 'zod';
 import { Prisma } from '@prisma/client';
 
